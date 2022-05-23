2109495c7ecacc1ca10302af4e7650cf8706d7d3
Juan Pablo Medina <juanpablomedina1997@gmail.com>
2022-05-23 14:56:09 -0300

    Proyecto 1.1

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ItemListContainer = ({tittle}) => {
+  return (
+    <>
+        <h1 className='text-center'>{tittle}</h1>
+    </>
+  )
+}
+
+export default ItemListContainer
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+
+
+
+
+
+const NavBar = () => {
+
+
+
+  const [data, setData] = useState([]);
+
+  const obtenerDatos = async () => {
+    const data = await fetch("https://run.mocky.io/v3/d3ad108c-df6d-4b19-96f5-5a08a8e5a3f9");
+    const res = await data.json();
+    setData(res);
+  };
+
+  useEffect(() => {
+    obtenerDatos();
+  }, []);
+
+
+  
+  
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-black" >
+            <div className="collapse navbar-collapse">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-auto">
+                    {
+                        data.map( item =>(
+                            <li className="nav-item" key={item.id}>
+                                <a className="nav-link active">
+                                    {<img src={item.img} width="50px" height="50px"/>}  {item.name}
+                                </a>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+    </nav>
+    </>
+  );
+};
+
+export default NavBar;
